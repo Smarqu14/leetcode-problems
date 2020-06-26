1801c12d4d726b73ddde18a29f99a3c7aacc0cd5
Steve Marquez <steveantonio00@gmail.com>
2020-06-26 12:19:05 -0700

    Implement better solution for same.js with time complexity of o(n)

--- same.js
+++ same.js
@@ -1,7 +1,7 @@
 /**
  Naive Approach
 
- 
+
  Write a function called same, whoch accepts two arrays/ The function should return true if every value
  in the array has its corresponding value squared int he second array. The frequency of values must be the same.
 
@@ -15,27 +15,65 @@ e: none
 TIME COMPLEXITY: 
 O(N^2); we are looping and we also have a nested loop (indexOf)
 
-
-
  */
 
-function same(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-
-  for (var i = 0; i < arr1.length; i++) {
-    let correctIndex = arr2.indexOf(arr1[i] ** 2);
-    // -1 means if its not in there 
-    if (correctIndex === -1) {
+ /**
+  * 
+  * NAIVE SOLUTION - TIME COMPLEXITY O(N^2)
+  * 
+  
+  function same(arr1, arr2) {
+    if (arr1.length !== arr2.length) {
       return false;
     }
-    // this line removes the number from the array
-    arr2.splice(correctIndex, 1);
+  
+    for (var i = 0; i < arr1.length; i++) {
+      let correctIndex = arr2.indexOf(arr1[i] ** 2);
+      // -1 means if its not in there 
+      if (correctIndex === -1) {
+        return false;
+      }
+      // this line removes the number from the array
+      arr2.splice(correctIndex, 1);
+    }
+    return true;
   }
-  return true;
-}
+  
+  console.log(same([1,2,3], [4,1,9])); // true
+  console.log(same([1,2,3], [1,9])); //false
+  console.log(same([1,2,1], [4,4,1])); //false (must be the same frequency)  
+  
+  
+  */
 
-console.log(same([1,2,3], [4,1,9])); // true
-console.log(same([1,2,3], [1,9])); //false
-console.log(same([1,2,1], [4,4,1])); //false (must be the same frequency)
+
+  // TIME COMPLEXITY O(N)
+
+  function same(arr1, arr2) {
+    var obj1 = {};
+    var obj2 = {};
+
+    // good for arrays/strings
+    for (var val of arr1) {
+      obj1[val] = ++obj1[val] || 1;
+    }
+    
+    // good for arrays/strings
+    for (var val of arr2) {
+      obj2[val] = ++obj2[val] || 1;
+    }
+    
+    for (var key in obj1) {
+      if (!(key ** 2 in obj2)) {
+        return false;
+      }
+      if (obj2[key ** 2] !== obj1[key]) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  console.log(same([1,2,3,2], [9,1,4,4])); // true
+  console.log(same([1,2,3], [1,9])); //false
+  console.log(same([1,2,1], [4,4,1])); //false (must be the same frequency)  
