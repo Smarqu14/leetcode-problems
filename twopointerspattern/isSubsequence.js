4b1dcac05433dff83fa6a16d6a23c029919eef4e
Steve Marquez <steveantonio00@gmail.com>
2020-07-01 17:23:30 -0700

    Implement a recursive approach to isSubsequence.jws

--- twopointerspattern/isSubsequence.js
+++ twopointerspattern/isSubsequence.js
@@ -16,12 +16,12 @@ C: time complexity
 E: 
   if (string1 is undefined),
 */
-
+// iterative approach
 function isSubsequence(str1, str2) {
   var i = 0;
   var j = 0;
   if (!str1) return true;
-  
+
   while (j < str2.length) {
     if (str2[j] === str1[i]) i++;
     if (i === str1.length) return true;
@@ -30,6 +30,13 @@ function isSubsequence(str1, str2) {
   return false;
 }
 
+// Recursive approach
+// function isSubsequence(str1, str2) {
+//   if(str1.length === 0) return true
+//   if(str2.length === 0) return false
+//   if(str2[0] === str1[0]) return isSubsequence(str1.slice(1), str2.slice(1))  
+//   return isSubsequence(str1, str2.slice(1))
+// }
 console.log(isSubsequence("hello", "hello world")); //true
 console.log(isSubsequence("sing", "sting")); //true
 console.log(isSubsequence("abc", "abracadabra")); //true
