a966fab340e0eed41546bbf635858475d269a26b
Steve Marquez <steveantonio00@gmail.com>
2020-07-02 17:13:50 -0700

    Add countDown to the recursion folder

---
+++ recursion/countDown.js
@@ -0,0 +1,13 @@
+// Our first recursive function
+
+function countDown(num) {
+  if (num <= 0) {
+    console.log("All done");
+    return;
+  }
+  console.log(num);
+  num--;
+  countDown(num);
+}
+
+countDown(3);
