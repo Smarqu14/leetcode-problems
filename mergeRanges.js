b360f91c766cd0fdaae634256f05b9f51feb1d97
Steve Marquez <steveantonio00@gmail.com>
2020-08-12 11:11:49 -0700

    Implement mergedRanges

---
+++ mergeRanges.js
@@ -0,0 +1,36 @@
+// Your company built an in-house calendar tool called HiCal. You want to add a feature to see the times in a day when everyone is available.
+
+// To do this, you’ll need to know when any team is having a meeting. In HiCal, a meeting is stored as objects ↴ with integer properties startTime and endTime. These integers represent the number of 30-minute blocks past 9:00am.
+
+function mergeRanges(meetings) {
+  // Create a deep copy of the meetings array
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/assign#Deep_Clone
+  const meetingsCopy = JSON.parse(JSON.stringify(meetings));
+
+  // Sort by start time
+  const sortedMeetings = meetingsCopy.sort((a, b) => {
+    return a.startTime - b.startTime;
+  });
+
+  // Initialize mergedMeetings with the earliest meeting
+  const mergedMeetings = [sortedMeetings[0]];
+
+  for (let i = 1; i < sortedMeetings.length; i++) {
+    const currentMeeting = sortedMeetings[i];
+    const lastMergedMeeting = mergedMeetings[mergedMeetings.length - 1];
+
+    // If the current meeting overlaps with the last merged meeting, use the
+    // later end time of the two
+    if (currentMeeting.startTime <= lastMergedMeeting.endTime) {
+      lastMergedMeeting.endTime = Math.max(
+        lastMergedMeeting.endTime,
+        currentMeeting.endTime
+      );
+    } else {
+      // Add the current meeting since it doesn't overlap
+      mergedMeetings.push(currentMeeting);
+    }
+  }
+
+  return mergedMeetings;
+}
