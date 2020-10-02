3db98de5adb4d17c9d4bedf2134ce5d017da71b7
Steve Marquez <steveantonio00@gmail.com>
2020-10-02 13:57:58 -0700

    implement two poineters for areThereDuplicates

--- udemy/areThereDuplicates.js
+++ udemy/areThereDuplicates.js
@@ -9,21 +9,36 @@ console.log(areThereDuplicates(1,2,2)) // true
 console.log(areThereDuplicates("a","b","c","a")) // true
 
 
-*/
-
+// Frequency pattern 
 const areThereDuplicates = () => {
   let collection = {};
-
+  
   for (let val in arguments) {
     collection[arguments[val]] = ++collection[arguments[val]] || 1;
   }
-
+  
   for (let key in collection) {
     if (collection[key] > 1) return true;
   }
   return false;
 };
+*/
+
+// Two pointers
+function areThereDuplicates(...args) {
+  args.sort((a, b) => a > b);
+  let start = 0;
+  let next = 1;
+  while (next < args.length) {
+    if (args[start] === args[next]) {
+      return true;
+    }
+    start++;
+    next++;
+  }
+  return false;
+}
 
 console.log(areThereDuplicates(1, 2, 3)); //false
 console.log(areThereDuplicates(1, 2, 2)); // true
-// console.log(areThereDuplicates("a", "b", "c", "a")); // true
+console.log(areThereDuplicates("a", "b", "c", "a")); // true
