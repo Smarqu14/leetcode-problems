e71decb79d3fb692ce29de03a105a016bbe58907
Steve Marquez <steveantonio00@gmail.com>
2020-10-08 14:16:35 -0700

    implement another solution for leetcode 3

--- 3. Longest Substring Without Repeating Characters.js
+++ 3. Longest Substring Without Repeating Characters.js
@@ -1,4 +1,3 @@
-
 /*
 3. Longest Substring Without Repeating Characters
 
@@ -33,25 +32,46 @@ Constraints:
 0 <= s.length <= 5 * 104
 s consists of English letters, digits, symbols and spaces.
 
+// using a for loop
+var lengthOfLongestSubstring = function(s) {
+    if (s.length === 0 || s === null) {
+        return 0;
+    }
+    
+    let seen = {};
+    let start = 0;
+    let substring = 0;
+    
+    for (let i = 0; i < s.length; i++) {
+        let char = s[i];
+        
+        if (seen[char]) {
+            start = Math.max(start, seen[char]);
+        }
+        substring = Math.max(substring, i - start + 1);
+        seen[char] = i + 1;
+    }
+    return substring;
+}
 */
 
-var lengthOfLongestSubstring = function(s) {
-  if (s.length === 0 || s === null) {
-      return 0;
+function lengthOfLongestSubstring(str) {
+  if (str.length === 0 || s === null) return 0;
+
+  let set = new Set();
+  let left = 0;
+  let right = 0;
+  let maxSubstringLength = 0;
+
+  while (right < str.length) {
+    if (!set.has(str.charAt(right))) {
+      set.add(str.charAt(right));
+      maxSubstringLength = Math.max(maxSubstringLength, set.size);
+      right++;
+    } else {
+      set.delete(str.charAt(left));
+      left++;
+    }
   }
-  
-  let seen = {};
-  let start = 0;
-  let substring = 0;
-  
-  for (let i = 0; i < s.length; i++) {
-      let char = s[i];
-      
-      if (seen[char]) {
-          start = Math.max(start, seen[char]);
-      }
-      substring = Math.max(substring, i - start + 1);
-      seen[char] = i + 1;
-  }
-  return substring;
+  return maxSubstringLength;
 }
