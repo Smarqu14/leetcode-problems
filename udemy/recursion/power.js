ec32180ed5de697d28f36b4d8d3c94c35287fdc3
Steve Marquez <steveantonio00@gmail.com>
2020-10-16 20:57:03 -0700

    implement factorial.js and O(logn) power.js

--- udemy/recursion/power.js
+++ udemy/recursion/power.js
@@ -1,13 +1,28 @@
 /*Recursion
 Write a function called power which accepts a base and an exponent. The function should return the power of the base to the exponent. This function should mimic the functionality of Math.pow() - Do not worry about negative bases and exponents
 
-*/
 
 const power = (base, exponent) => {
   if (exponent === 0) {
     return 1;
   }
-
+  
   return base * power(base, exponent - 1) ;
+  
+}; 
 
+O(n);
+*/
+
+const power = (base, exponent) => {
+  if (exponent === 0) {
+    return 1;
+  } else if (exponent % 2 === 0) {
+    let y = power(base, exponent / 2);
+    return y * y;
+  } else {
+    return base * power(base, exponent - 1);
+  }
 };
+
+//O(log n)
