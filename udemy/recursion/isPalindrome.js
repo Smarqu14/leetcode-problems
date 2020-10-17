2b33ff4262269d423421ba352399e0c8882cd02e
Steve Marquez <steveantonio00@gmail.com>
2020-10-17 11:38:55 -0700

    create isPalindrome file and implement solution 2 for reverse.js

---
+++ udemy/recursion/isPalindrome.js
@@ -0,0 +1,15 @@
+/*
+Recursive - Write a recursive function called isPalindrome which returns true if the string passed to its a palindrome (reads the same fowward and backward). Othewise return false;
+
+
+// isPalindrome('awesome') // false
+// isPalindrome('foobar') // false
+// isPalindrome('tacocat') // true
+// isPalindrome('amanaplanacanalpanama') // true
+// isPalindrome('amanaplanacanalpandemonium') // false
+
+*/
+
+function isPalindrome(){
+  // add whatever parameters you deem necessary - good luck!
+}
