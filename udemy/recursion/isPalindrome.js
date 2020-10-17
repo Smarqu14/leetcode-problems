76f27230b5725bf31a190463cedad50ee733fd0c
Steve Marquez <steveantonio00@gmail.com>
2020-10-17 11:51:44 -0700

    Implement isPalindrome.js with recursion

--- udemy/recursion/isPalindrome.js
+++ udemy/recursion/isPalindrome.js
@@ -10,6 +10,10 @@ Recursive - Write a recursive function called isPalindrome which returns true if
 
 */
 
-function isPalindrome(){
+function isPalindrome(str){
   // add whatever parameters you deem necessary - good luck!
+  if (str.length <= 1) return true;
+  if (str[0] !== str[str.length -1]) return false;
+
+  return isPalindrome(str.slice(1, -1));
 }
