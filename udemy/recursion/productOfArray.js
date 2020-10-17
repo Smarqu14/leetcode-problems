cc0dcfc990f3e936d3ca0bcf24609bb9f7ca9f80
Steve Marquez <steveantonio00@gmail.com>
2020-10-17 11:24:41 -0700

    implement reverse.js with recursion

--- udemy/recursion/productOfArray.js
+++ udemy/recursion/productOfArray.js
@@ -1,12 +1,20 @@
 /* 
 Recursion- Write a function called productOfArray which takes in an array of numbers and returns the product of them all.
-*/
-
+// Solution 1
 const productOfArray = (nums) => {
   if (nums.length === 0) {
     return 1;
   }
-
+  
   return nums.shift() * productOfArray(nums);
 };
+*/
 
+// Solution 2
+
+function productOfArray(arr) {
+  if (arr.length === 0) {
+    return 1;
+  }
+  return arr[0] * productOfArray(arr.slice(1));
+}
---
+++ udemy/recursion/reverse.js
@@ -0,0 +1,16 @@
+/*
+Recursive - Write a recursive function called reverse which accepts a string and returns a new string in reverse.
+
+// reverse('awesome') // 'emosewa'
+// reverse('rithmschool') // 'loohcsmhtir'
+*/
+
+
+function reverse(str) {
+  if (str.length <= 1) {
+    return str;
+  }
+  return reverse(str.slice(1));
+
+
+};
